0416587cea02f4a1daa20759fdb3e23355764a54
fadnihdr <fadnihdr@gmail.com>
2016-05-24 13:04:33 +0800

    added input from user added conditions

--- samplecode.py
+++ samplecode.py
@@ -1,6 +1,14 @@
 def greet(name):
-    print 'Hello', name
-greet('Jack')
-greet('Jill')
-greet('Bob')
+    if name == "Fadni":
+        print ('Hello', name, "The coolest guy around")
+    elif name == "Diraj":
+        print ('Hello', name, "Whose memes are mediocre")
+    else:
+        print('Hello', name, "Have a nice day")
 
+name = input("What name?")
+
+greet(name)
+
+#  Reviewed by Fadni
+#  added input from user, and conditions.
